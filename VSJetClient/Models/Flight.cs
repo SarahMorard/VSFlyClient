5b406f33d887cbc64ff99cf20259ffe6f00854b0
Sarah <sarah.morard@students.hevs.ch>
2020-06-01 15:36:33 +0200

    Base price

--- VSJetClient/Models/Flight.cs
+++ VSJetClient/Models/Flight.cs
@@ -14,6 +14,7 @@ namespace VSJetClient.Models
         public DateTime Date { get; set; }
 
         public int Seats { get; set; }
+        public double BasePrice { get; set; }
         public bool isComplete { get; set; }
     }
 }
